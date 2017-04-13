5e35366908f9dd1bff559caed613ab2fbdfae3db
Shaan Chopra <shaanchopra@Shaans-MacBook-Pro.local>
2017-04-14 01:04:40 +0530

    Gui for complex queries updated

--- SystemHAS.java
+++ SystemHAS.java
@@ -16,8 +16,12 @@ public class SystemHAS{
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
-	JButton bttn_str, bttn_exit, bttn_register;
+	JButton bttn_str, bttn_exit, bttn_register, query1, query2, query3, query4, query5;
 	String stream_select="";
+	String room_select="";
+	String sex_select="";
+	String room_type1="";
+	String room_type2="";
 	SystemHAS(){
 		frame=new JFrame();
 		panel=new JPanel();
@@ -26,6 +30,11 @@ public class SystemHAS{
 		bttn_str=new JButton("Login");
 		bttn_exit=new JButton("Exit");
 		bttn_register=new JButton("Register");
+		query1=new JButton("Query 1");
+		query2=new JButton("Query 2");
+		query3=new JButton("Query 3");
+		query4=new JButton("Query 4");
+		query5=new JButton("Query 5");
 
 		bttn_str.setBackground(Color.CYAN);
 		bttn_str.setOpaque(true);
@@ -39,16 +48,46 @@ public class SystemHAS{
 		bttn_register.setOpaque(true);
 		bttn_register.setBorderPainted(false);
 
+		query1.setBackground(Color.CYAN);
+		query1.setOpaque(true);
+		query1.setBorderPainted(false);
+
+		query2.setBackground(Color.CYAN);
+		query2.setOpaque(true);
+		query2.setBorderPainted(false);
+
+		query3.setBackground(Color.CYAN);
+		query3.setOpaque(true);
+		query3.setBorderPainted(false);
+
+		query4.setBackground(Color.CYAN);
+		query4.setOpaque(true);
+		query4.setBorderPainted(false);
+
+		query5.setBackground(Color.CYAN);
+		query5.setOpaque(true);
+		query5.setBorderPainted(false);
+
 		panel.setLayout(null);
 		label.setBounds(200,120,500,40);
 		label.setFont(new Font("Arial",Font.BOLD,30));
 		//bttn_str.setBounds(330,250,150,40);
 		bttn_str.setBounds(330,250,150,40);
-		bttn_register.setBounds(350,350,100,40);
-		bttn_exit.setBounds(350,450,100,40);
+		bttn_register.setBounds(350,300,100,40);
+		query1.setBounds(350,350,100,40);
+		query2.setBounds(350,400,100,40);
+		query3.setBounds(350,450,100,40);
+		query4.setBounds(350,500,100,40);
+		query5.setBounds(350,550,100,40);
+		bttn_exit.setBounds(350,600,100,40);
 		panel.add(label);
 		panel.add(bttn_str);
 		panel.add(bttn_register);
+		panel.add(query1);
+		panel.add(query2);
+		panel.add(query3);
+		panel.add(query4);
+		panel.add(query5);
 		panel.add(bttn_exit);
 		panel.setBackground(new Color(255,255,244));
 		bttn_str.addActionListener(new ActionListener(){
@@ -59,6 +98,31 @@ public class SystemHAS{
 		bttn_register.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ev){
 				new Register_Form();
+			}
+		});
+		query1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				new Query1_form();
+			}
+		});
+		query2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				new Query2_form();
+			}
+		});
+		query3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				new Query3_form();
+			}
+		});
+		query4.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				new Query4_form();
+			}
+		});
+		query5.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){
+				new Query5_form();
 			}
 		});
 		bttn_exit.addActionListener(new ActionListener(){
@@ -369,6 +433,364 @@ public class SystemHAS{
 	}
 
 	public class View_Student{
+
+	}
+
+	public class Query1_form{
+		Query1_form(){
+			panel.removeAll();
+			panel.updateUI();
+			panel.setLayout(new FlowLayout());
+			JPanel panel2=new JPanel();
+			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+			panel2.setBackground(new Color(255,255,244));
+			JPanel panel1=new JPanel();
+			panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+			panel1.setBackground(new Color(255,255,244));
+			panel2.setPreferredSize(new Dimension(200, 100));
+			panel1.setPreferredSize(new Dimension(300, 100));
+			JButton hname=new JButton("Hostel Name");
+			JButton type=new JButton("Room Type");
+			JButton back=new JButton("Back");
+
+			JTextField f=new JTextField();
+			f.setPreferredSize(new Dimension(50, 120));
+
+			JRadioButton single_s = new JRadioButton("Single");
+      		JRadioButton double_s = new JRadioButton("Double Sharing");
+      		JRadioButton triple_s = new JRadioButton("Triple Sharing");
+			ButtonGroup group=new ButtonGroup();
+			group.add(single_s);
+			group.add(double_s);
+			group.add(triple_s);
+
+			panel2.add(hname);
+			panel2.add(type);
+			panel1.add(f);
+			panel1.add(single_s);
+			panel1.add(double_s);
+			panel1.add(triple_s);
+			panel.add(panel2);
+			panel.add(panel1);
+			back.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ev){
+					new SystemHAS();
+				}
+			});
+			single_s.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_select="single";
+         		}           
+      		});
+      		double_s.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_select="double";
+         		}           
+      		});
+      		triple_s.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_select="triple";
+         		}           
+      		});
+
+      		JButton submit=new JButton("Submit");
+      		submit.addActionListener(new ActionListener(){
+      			public void actionPerformed(ActionEvent ev){
+
+      			}
+      		});
+      		panel.add(submit);
+      		panel.add(back);
+      		frame.getContentPane().add(panel,BorderLayout.CENTER);
+		}
+		public int check_hname_in_database(String h){
+			
+			//if(hi is present in the hostel database)
+			//return 1
+			//else
+			return 0;
+		}
+	}
+	public class Query2_form{
+		Query2_form(){
+			panel.removeAll();
+			panel.updateUI();
+			panel.setLayout(new FlowLayout());
+			JPanel panel2=new JPanel();
+			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+			panel2.setBackground(new Color(255,255,244));
+			JPanel panel1=new JPanel();
+			panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+			panel1.setBackground(new Color(255,255,244));
+			panel2.setPreferredSize(new Dimension(200, 100));
+			panel1.setPreferredSize(new Dimension(300, 100));
+			JButton floor=new JButton("Floor");
+			JButton sex1=new JButton("Sex");
+			JButton back=new JButton("Back");
+
+			JTextField f=new JTextField();
+			f.setPreferredSize(new Dimension(50, 120));
+			JRadioButton male = new JRadioButton("Male");
+      		JRadioButton female = new JRadioButton("Female");
+      		JRadioButton other = new JRadioButton("Other");
+			ButtonGroup group=new ButtonGroup();
+			group.add(male);
+			group.add(female);
+			group.add(other);
+
+			panel2.add(floor);
+			panel2.add(sex1);
+			panel1.add(f);
+			panel1.add(male);
+			panel1.add(female);
+			panel1.add(other);
+			panel.add(panel2);
+			panel.add(panel1);
+			back.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ev){
+					new SystemHAS();
+				}
+			});
+			male.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			sex_select="male";
+         		}           
+      		});
+      		female.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			sex_select="female";
+         		}           
+      		});
+      		other.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			sex_select="other";
+         		}           
+      		});
+			JButton submit=new JButton("Submit");
+      		submit.addActionListener(new ActionListener(){
+      			public void actionPerformed(ActionEvent ev){
+
+      			}
+      		});
+      		panel.add(submit);
+      		panel.add(back);
+      		frame.getContentPane().add(panel,BorderLayout.CENTER);
+		}
+
+		public int check_floor(String fl){
+			int flo=Integer.parseInt(fl);
+			//apply check if exists in database or not
+			return 0;
+		}
+
+	}
+
+	public class Query3_form{
+		Query3_form(){
+			panel.removeAll();
+			panel.updateUI();
+			panel.setLayout(new FlowLayout());
+			JPanel panel2=new JPanel();
+			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+			panel2.setBackground(new Color(255,255,244));
+			JPanel panel1=new JPanel();
+			panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+			panel1.setBackground(new Color(255,255,244));
+			panel2.setPreferredSize(new Dimension(200, 30));
+			panel1.setPreferredSize(new Dimension(300, 30));
+			JButton mess1=new JButton("Mess ID");
+			JButton submit=new JButton("Submit");
+			JButton back=new JButton("Back");
+			JTextField f=new JTextField();
+			f.setPreferredSize(new Dimension(50, 120));
+
+			panel2.add(mess1);
+			panel1.add(f);
+			panel.add(panel2);
+			panel.add(panel1);
+			back.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ev){
+					new SystemHAS();
+				}
+			});
+      		submit.addActionListener(new ActionListener(){
+      			public void actionPerformed(ActionEvent ev){
+
+      			}
+      		});
+      		panel.add(submit);
+      		panel.add(back);
+      		frame.getContentPane().add(panel,BorderLayout.CENTER);
+
+		}
+		public int check_messid_in_database(String m){
+			int mi=Integer.parseInt(m);
+			//if(mi is present in the mess database)
+			//return 1
+			//else
+			return 0;
+		}
+
+	}
+	public class Query4_form{
+		Query4_form(){
+			panel.removeAll();
+			panel.updateUI();
+			panel.setLayout(new FlowLayout());
+			JPanel panel2=new JPanel();
+			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+			panel2.setBackground(new Color(255,255,244));
+			JPanel panel1=new JPanel();
+			panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+			panel1.setBackground(new Color(255,255,244));
+			panel2.setPreferredSize(new Dimension(200, 100));
+			panel1.setPreferredSize(new Dimension(300, 150));
+			JButton type1=new JButton("Room 1 Type");
+			JButton type2=new JButton("Room 2 Type");
+			JButton submit=new JButton("Submit");
+			JButton back=new JButton("Back");
+
+			JRadioButton single_s1 = new JRadioButton("Single");
+      		JRadioButton double_s1 = new JRadioButton("Double Sharing");
+      		JRadioButton triple_s1 = new JRadioButton("Triple Sharing");
+			ButtonGroup group1=new ButtonGroup();
+			group1.add(single_s1);
+			group1.add(double_s1);
+			group1.add(triple_s1);
+
+			JRadioButton single_s2 = new JRadioButton("Single");
+      		JRadioButton double_s2 = new JRadioButton("Double Sharing");
+      		JRadioButton triple_s2 = new JRadioButton("Triple Sharing");
+      		ButtonGroup group2=new ButtonGroup();
+			group2.add(single_s2);
+			group2.add(double_s2);
+			group2.add(triple_s2);
+
+			panel2.add(type1);
+			panel2.add(type2);
+			panel1.add(single_s1);
+			panel1.add(double_s1);
+			panel1.add(triple_s1);
+			panel1.add(single_s2);
+			panel1.add(double_s2);
+			panel1.add(triple_s2);
+			panel.add(panel2);
+			panel.add(panel1);
+			back.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ev){
+					new SystemHAS();
+				}
+			});
+			single_s1.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type1="single";
+         		}           
+      		});
+      		double_s1.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type1="double";
+         		}           
+      		});
+      		triple_s1.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type1="triple";
+         		}           
+      		});
+      		single_s2.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type2="single";
+         		}           
+      		});
+      		double_s2.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type2="double";
+         		}           
+      		});
+      		triple_s2.addItemListener(new ItemListener() {
+         		public void itemStateChanged(ItemEvent ev) {         
+            		if(ev.getStateChange()==1)
+            			room_type2="triple";
+         		}           
+      		});
+      		submit.addActionListener(new ActionListener(){
+      			public void actionPerformed(ActionEvent ev){
+      				if(check_same_type(room_type1,room_type2)==1){
+      					//throw an error
+      				}
+      			}
+      		});
+      		panel.add(submit);
+      		panel.add(back);
+      		frame.getContentPane().add(panel,BorderLayout.CENTER);
+		}
+		public int check_same_type(String a, String b){
+			if(a.equals(b)){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}
+
+	}
+	public class Query5_form{
+		Query5_form(){
+			panel.removeAll();
+			panel.updateUI();
+			panel.setLayout(new FlowLayout());
+			JPanel panel2=new JPanel();
+			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+			panel2.setBackground(new Color(255,255,244));
+			JPanel panel1=new JPanel();
+			panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+			panel1.setBackground(new Color(255,255,244));
+			panel2.setPreferredSize(new Dimension(200, 30));
+			panel1.setPreferredSize(new Dimension(300, 30));
+			JButton hid=new JButton("Hostel ID");
+			JButton submit=new JButton("Submit");
+			JButton back=new JButton("Back");
+
+			JTextField f=new JTextField();
+			f.setPreferredSize(new Dimension(50, 120));
+
+			panel2.add(hid);
+			panel1.add(f);
+			panel.add(panel2);
+			panel.add(panel1);
+			back.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ev){
+					new SystemHAS();
+				}
+			});
+      		submit.addActionListener(new ActionListener(){
+      			public void actionPerformed(ActionEvent ev){
+
+      			}
+      		});
+      		panel.add(submit);
+      		panel.add(back);
+      		frame.getContentPane().add(panel,BorderLayout.CENTER);
+
+		}
+
+		public int check_hid_in_database(String h){
+			int hi=Integer.parseInt(h);
+			//if(hi is present in the hostel database)
+			//return 1
+			//else
+			return 0;
+		}
 
 	}
 
